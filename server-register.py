b6baf39192d4300cecd9cfb419834eb3b649754c
soybean217 <13565644@qq.com>
2017-03-06 18:16:34 +0800

    add log to database

--- server-register.py
+++ server-register.py
@@ -5,39 +5,52 @@ import tornado.web
 import struct
 import torndb
 import time
-import geoip2.database
+import sys
+import random
 
+#private lib
 import config
 from Bastion import _test
 
-TEST_CONTENT =  "<datas><cfg><durl></durl><vno></vno><stats>1</stats></cfg><da><data><kno>135</kno><kw>验证码*中国铁路</kw><apid>100</apid></data></da></datas>";
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+TEST_CONTENT =  "<datas><cfg><durl></durl><vno></vno><stats>1</stats></cfg><da><data><kno>106</kno><kw>验证码*。</kw><apid>100</apid></data><data><kno>135</kno><kw>验证码*。</kw><apid>100</apid></data></da></datas>";
+SUCCESS_CONTENT = "<datas><stats>1</stats></datas>"
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("")
-        print("begin")
-        # yield sleep_test()
-        res = yield self.sleep()
-        self.write("when i sleep %f s" % (time.time() - start))
-        print("end")
+
     def post(self, *args, **kwargs):
         self.write("")  
-
-    def sleep(self):
-        time.sleep(5)
-        return 5
-
 
         
 class GetCHandler(tornado.web.RequestHandler):
     def get(self):
         self.write(TEST_CONTENT)
+        print('getc imsi:'+str(self.get_argument('imsi')))
 
 class SendCHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("")
-        print(self.get_argument('msg'))
-        print(self.get_argument('code'))
+        self.write(SUCCESS_CONTENT)
+        _msg = str(self.get_argument('msg'))
+        _sendInfo = {}
+        _sendInfo["msg"] = str(self.get_argument('msg'))
+        _sendInfo["code"] = str(self.get_argument('code'))
+        _sendInfo["imsi"] = str(self.get_argument('imsi'))
+        _sendInfo["cid"] = str(self.get_argument('cid'))
+        _sendInfo["apid"] = str(self.get_argument('apid'))
+        print('sendinfo:'+str(_sendInfo))
+        print('msg origin:'+_sendInfo["msg"])
+        print('msg decode:'+str(_sendInfo["msg"].decode))
+        insert_register_send_log(_sendInfo)
+
+def insert_register_send_log(_info):
+    dbLog=torndb.Connection(config.GLOBAL_SETTINGS['log_db']['host'],config.GLOBAL_SETTINGS['log_db']['name'],config.GLOBAL_SETTINGS['log_db']['user'],config.GLOBAL_SETTINGS['log_db']['psw'])
+    sql = 'insert into log_async_generals (`id`,`logId`,`para01`,`para02`,`para03`,`para04`,`para05`) values (%s,%s,%s,%s,%s,%s,%s)'
+    dbLog.insert(sql,long(round(time.time() * 1000))*10000+random.randint(0, 9999),311,_info["imsi"],_info["msg"],_info["code"],_info["cid"],_info["apid"])
+    return 
 
 def make_app():
     return tornado.web.Application([
@@ -46,13 +59,8 @@ def make_app():
         (r"/ss/sendc", SendCHandler),
     ])
 
-def sleep_test():
-    time.sleep(3)
-    print("sleep finish")
-    return 3
-
 if __name__ == "__main__":
-    print "begin..."
+    print("register begin...")
     app = make_app()
     app.listen(config.GLOBAL_SETTINGS['port'])
     tornado.ioloop.IOLoop.current().start()
