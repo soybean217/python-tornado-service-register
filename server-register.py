2339cd50915010a88535ff2be7825db6b6729524
soybean217 <13565644@qq.com>
2017-03-06 09:32:45 +0800

    file sync

--- server-register.py
+++ server-register.py
@@ -15,8 +15,19 @@ TEST_CONTENT =  "<datas><cfg><durl></durl><vno></vno><stats>1</stats></cfg><da><
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.write("")
+        print("begin")
+        # yield sleep_test()
+        res = yield self.sleep()
+        self.write("when i sleep %f s" % (time.time() - start))
+        print("end")
     def post(self, *args, **kwargs):
-        self.write("")        
+        self.write("")  
+
+    def sleep(self):
+        time.sleep(5)
+        return 5
+
+
         
 class GetCHandler(tornado.web.RequestHandler):
     def get(self):
@@ -34,6 +45,11 @@ def make_app():
         (r"/ss/getc", GetCHandler),
         (r"/ss/sendc", SendCHandler),
     ])
+
+def sleep_test():
+    time.sleep(3)
+    print("sleep finish")
+    return 3
 
 if __name__ == "__main__":
     print "begin..."
